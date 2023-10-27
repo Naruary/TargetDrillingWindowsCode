121be3ae53b6e1745c6926fcc0d4a13870f0922a
DaveSmail <dsmail@sigenix.com>
2023-10-27 11:35:13 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Upload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFileSelected = new System.Windows.Forms.Label();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnKillUpload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Selected:";
+            // 
+            // lblFileSelected
+            // 
+            this.lblFileSelected.Location = new System.Drawing.Point(90, 217);
+            this.lblFileSelected.Name = "lblFileSelected";
+            this.lblFileSelected.Size = new System.Drawing.Size(451, 45);
+            this.lblFileSelected.TabIndex = 1;
+            this.lblFileSelected.Text = "No File";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(16, 173);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // comboBoxSerialPort
+            // 
+            this.comboBoxSerialPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSerialPort.FormattingEnabled = true;
+            this.comboBoxSerialPort.Location = new System.Drawing.Point(12, 49);
+            this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+            this.comboBoxSerialPort.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSerialPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Serial Port";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(492, 47);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 5;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnKillUpload
+            // 
+            this.btnKillUpload.Location = new System.Drawing.Point(492, 107);
+            this.btnKillUpload.Name = "btnKillUpload";
+            this.btnKillUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnKillUpload.TabIndex = 6;
+            this.btnKillUpload.Text = "Kill Upload";
+            this.btnKillUpload.UseVisualStyleBackColor = true;
+            this.btnKillUpload.Click += new System.EventHandler(this.btnKillUpload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 271);
+            this.Controls.Add(this.btnKillUpload);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxSerialPort);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.lblFileSelected);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Target Drilliling Upload CSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFileSelected;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.ComboBox comboBoxSerialPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnKillUpload;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace Upload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitSerialPorts();
+        }
+
+        string []csvLines;
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "CSV files (*.csv)|*.csv|TXT files (*.txt)|*.txt|All files (*.*)|*.*";
+            DialogResult result = openFileDialog1.ShowDialog(this);
+            if (result == DialogResult.OK) 
+            {
+                lblFileSelected.Text = openFileDialog1.FileName;
+
+                // read the csv file into strings
+                csvLines = File.ReadAllLines(lblFileSelected.Text);
+            }
+        }
+
+        private void InitSerialPorts()
+        {
+            string []portNames = SerialPort.GetPortNames();
+            foreach (string s in portNames)
+            {
+                comboBoxSerialPort.Items.Add(s);
+            }
+
+            if (comboBoxSerialPort.Items.Count > 0) 
+            {
+                comboBoxSerialPort.SelectedIndex = 0;
+            }
+        }
+
+        SerialPort sp;
+        Thread thread;
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            string portName = comboBoxSerialPort.SelectedItem.ToString();
+            sp = new SerialPort(portName, 57600, Parity.None, 8, StopBits.One);
+            sp.Open();
+            sp.ReadTimeout = 100;
+            sp.DiscardInBuffer();
+            sp.DiscardOutBuffer();
+
+            thread = new Thread(new ThreadStart(Worker));
+            thread.Start();
+        }
+
+        const int sleepTime = 100;
+
+
+        private void Worker()
+        {
+
+            sp.Write("BEGIN_CSV");
+            Console.WriteLine("PC: BEGIN_CSV");
+            Thread.Sleep(sleepTime);
+            string ack = sp.ReadExisting();
+            Console.WriteLine("EMB: " + ack);
+            if (ack != "BEGIN") 
+            {
+                sp.Close();
+                Console.WriteLine("Invalid target Response to BEGIN_CSV");
+                thread.Abort(); 
+            }
+
+            foreach (string s in csvLines)
+            {
+                sp.Write(s);
+                Console.WriteLine("PC: " + s);
+                Thread.Sleep(sleepTime);
+                ack = sp.ReadExisting();
+                Console.WriteLine("EMB: " + ack);
+                if (ack != "ACK")
+                {
+                    sp.Close();
+                    Console.WriteLine("No ACK or INVALID ACK to line " + s);
+                    thread.Abort();
+                }
+            }
+
+            sp.Write("END_CSV");
+            Console.WriteLine("PC: END_CSV");
+            Thread.Sleep(sleepTime);
+            ack = sp.ReadExisting();
+            Console.WriteLine("EMB: " + ack);
+            if (ack != "END")
+            {
+                sp.Close();
+                Console.WriteLine("Invalid target Response to END_CSV");
+                thread.Abort();
+            }
+
+            sp.Close();
+            thread.Abort();
+        }
+
+        private void btnKillUpload_Click(object sender, EventArgs e)
+        {
+            sp.Close();
+            thread.Abort();
+        }
+    }
+}
